99d595164841442db77490369521f089b1cef6e7
Edward Chen <kreychek@gmail.com>
2014-06-04 15:06:31 -0400

    Splitting up settings.py -> base.py + settings.py. settings.py is dev/local settings. base.py is settings that apply to all configs.

---
+++ lol_stats/base.py
@@ -0,0 +1,104 @@
+"""
+Django settings for lol_stats project that apply to all configurations.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+import os
+import sys
+
+from django.core.exceptions import ImproperlyConfigured
+
+from riotwatcher import riotwatcher
+
+
+def get_env_variable(var_name):
+    """
+    Get the environment variable or return exception.
+    """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = u'Set the {} environment variable'.format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
+
+sys.path.insert(0, os.getcwd())
+sys.path.insert(0, os.path.join(os.getcwd(), os.pardir))
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Following keys are stored as environment vars and in try blocks so RTD doesn't scream.
+
+# my Riot API key (keep secret!)
+riot_api = riotwatcher.RiotWatcher(get_env_variable('RIOT_API_KEY'))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
+
+ROOT_URLCONF = 'lol_stats.urls'
+
+WSGI_APPLICATION = 'lol_stats.wsgi.application'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+    'rest_framework',
+    'api',
+    'summoner',
+    'champion',
+    'item',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+# was True previously (default now?)
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    #'/usr/share/nginx/www/static/'
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'lol_stats/templates'),
+)
+
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 10,
+}
--- lol_stats/settings.py
+++ lol_stats/settings.py
@@ -1,5 +1,6 @@
 """
 Django settings for lol_stats project.
+This is presently dev/local settings.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -8,74 +9,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os, sys
-
-from riotwatcher import riotwatcher
-
-sys.path.insert(0, os.getcwd())
-sys.path.insert(0, os.path.join(os.getcwd(), os.pardir))
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-# Following keys are stored as environment vars and in try blocks so RTD doesn't scream.
-
-# my Riot API key (keep secret!)
-try:
-    riot_api = riotwatcher.RiotWatcher(os.environ['RIOT_API_KEY'])
-except KeyError:
-    pass
-
-# SECURITY WARNING: keep the secret key used in production secret!
-try:
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-except KeyError:
-    pass
+from base import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += (
     'debug_toolbar',
-    'django_extensions',
-    'rest_framework',
-    'api',
-    'summoner',
-    'champion',
-    'item',
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+MIDDLEWARE_CLASSES += (
+    # If debug_toolbar is installed app, but not included here, auto adds to front of list (good)
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
-
-ROOT_URLCONF = 'lol_stats.urls'
-
-WSGI_APPLICATION = 'lol_stats.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
@@ -87,39 +41,6 @@ DATABASES = {
     }
 }
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-# was True previously (default now?)
-USE_TZ = False
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    #'/usr/share/nginx/www/static/'
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'lol_stats/templates'),
-)
-
-REST_FRAMEWORK = {
-    'PAGINATE_BY': 10,
-}
-
 # Set to IPs you will be viewing the site from that you want debug_toolbar to appear for.
-#INTERNAL_IPS = ('10.0.2.2',)
+INTERNAL_IPS = ('127.0.0.1',
+                '10.0.2.2',)    # Address for host as seen from guest VM
